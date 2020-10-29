adcac645d5010d68ea442af18fa8b57ff0e11f61
KHong <albtrossfacefall@gmail.com>
2020-10-29 07:45:12 -0400

    start to implement display of opening text

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -4,21 +4,52 @@
 ///////////////////////////////////////////////////////////////////////////////////////////////
 ////////////////                                                                        IMPORTS
 // 1. React
-// 2. Styles
+// 2. Data
+// 3. Styles
 
 import React from "react";
+
+import openingTextJson from "../data/openingText.json";
 
 ///////////////////////////////////////////////////////////////////////////////////////////////
 ////////////////                                                                          SETUP
 
+const actorsInScene = ["The Fog", "Willow"] as const;
+
+type DialogueLineType = {
+  speaker: typeof actorsInScene[number]
+  text: string;
+};
+
+type OpeningTextJsonType = DialogueLineType[];
 
 ///////////////////////////////////////////////////////////////////////////////////////////////
 ////////////////                                                             COMPONENTS & LOGIC
 
-const Home = () => (
-  <div>
-    I'm home
-  </div>
-);
+const OpeningText = () => {
+  const openTextArr = openingTextJson as OpeningTextJsonType;
+  const displayDialogue = (dialogueLine: DialogueLineType) => {
+    return (
+      <div className={`openingTextDialogue`}>
+        <p>{dialogueLine.speaker}</p>
+        <p>{dialogueLine.text}</p>
+      </div>
+    )
+  };
+
+  return (
+    <div className="openingTextContainer">
+      {openTextArr.map((dialogueLine: DialogueLineType) => displayDialogue(dialogueLine))}
+    </div>
+  )
+}
+
+const Home = () => {
+  return (
+    <div className="homeViewRoot">
+      <OpeningText />
+    </div>
+  )
+}
 
 export default Home;
